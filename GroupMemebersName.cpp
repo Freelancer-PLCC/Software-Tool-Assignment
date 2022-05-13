d2ba1398e72e183f3bb723ec2a7404552100bf95
Beamlak Samson <beamlaksamson99@gmail.com>
2022-05-13 04:07:13 -0700

    Edited By Daniel Kebede

---
+++ GroupMemebersName.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Group memebers name";
+cout<<"Beamlak Samson	1201083";
+cout<<"Bewket Getaneh	1201197";
+cout<<"Daniel Kebede	1103170";
+cout<<"Lyu alemayehu	1202238";
+cout<<"Sireat Miheretu	1203295";
+return 0;
+}
